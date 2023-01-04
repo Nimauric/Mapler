c54873f7fe3d64fe282d9ea0a6f193889f7f0157
Nicolas Maurice <nimauric@teck.irisa.fr>
2023-01-04 15:09:47 +0100

    automatisation du téléchargement des séquences

--- shotgun_sequences_downloader.sh
+++ shotgun_sequences_downloader.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# Ce script télécharge chaque shotgun sequence dont les numéros d'accession
+# sont écrits dans SraAccList.txt
+
 input="./SraAccList.txt"
 while IFS= read -r line
 do
-  echo "$line"
-done
+    echo "downloading" "$line""..."
+    fasterq-dump "$line"
+done < "$input"
+echo "done !"
+
