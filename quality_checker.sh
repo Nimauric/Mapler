3f02736e6912141e63845c8eb033047bf6b7ea63
Nicolas Maurice <nimauric@genossh.genouest.org>
2023-01-06 16:43:25 +0100

    ne fait qu'un seul quality check par fastq

--- quality_checker.sh
+++ quality_checker.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-files = "./fastq/*.fastq"
-for f in files
+
+#SBATCH --mail-user=$nicolas.maurice@inria.fr
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+files=(./fastq/*.fastq)
+for f in "${files[@]}"
 do
- echo "Processing $f"
+    filename=`basename $f .fastq`
+    if ! [ -f ./fastQC/${filename}_fastqc.html ]
+        then
+	    fastqc $f -o ./fastQC
+        else
+            echo "$filename already analysed"
+    fi
+    
 done
