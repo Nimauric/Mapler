a1b322f6540dfbb9fc6f562db63e927037ff3876
Nicolas Maurice <nimauric@teck.irisa.fr>
2023-01-16 08:59:49 +0100

    metadata_fetcher takes the accession number as an argument

--- metadata_fetcher.sh
+++ metadata_fetcher.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-INPUT="SRR8073713"
+INPUT=$1
 
 # search the element from its accession name V
 XML="curl 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=sra&term=$INPUT' -s" 
 
-# parse the id in the resulting xml file X
+# get the id in the resulting xml file X
 ID="$XML | xml sel -t -v '//Id'"
 ID=$(eval "$ID")
 
-#get the xml of the run from its id
+# get the xml of the run from its id
 XML2="curl 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sra&id=$ID'"
 
-#get the sequencer from that xml
+# get the sequencer from that xml
 PLATFORM="$XML2 | xml sel -t -v '//INSTRUMENT_MODEL'"
-
-# run the command
-RESULT=$(eval "$PLATFORM")
+PLATFORM=$(eval "$PLATFORM")
 
 
 
-echo $RESULT
+echo $PLATFORM
