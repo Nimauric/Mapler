8065c52b36ccd0b27e7a46ce8df89dcd13ca5bfa
Nicolas Maurice <nimauric@cl1n037.genouest.org>
2023-01-17 09:41:32 +0100

    automatise metaquast_caller

--- metaquast_caller.sh
+++ metaquast_caller.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-metaquast ../GCA_003957645.1_BMock12_scaffolds_ILLUMINA_genomic.fna.gz
+files=(./assembly/*.f*)
+for f in "${files[@]}"
+do
+    echo $f
+    metaquast $f
+done
+
+#metaquast ../GCA_003957645.1_BMock12_scaffolds_ILLUMINA_genomic.fna.gz
 #metaquast assembly/assembly.fasta
