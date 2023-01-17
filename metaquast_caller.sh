88720ec866148e1737ab92e326478ca56fcc69f5
Nicolas Maurice <nimauric@cl1n037.genouest.org>
2023-01-17 09:46:02 +0100

    automatised metaquast with reference genome

--- metaquast_caller.sh
+++ metaquast_caller.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 source  ./env_init.sh
 
-<<<<<<< HEAD
 files=(./assembly/*.f*)
 for f in "${files[@]}"
 do
     echo $f
-    metaquast $f
+    metaquast $f -r $( echo reference_genomes/*/*/QC_and_Genome_Assembly/final.assembly.fasta | tr ' ' , )
 done
 
 #metaquast ../GCA_003957645.1_BMock12_scaffolds_ILLUMINA_genomic.fna.gz
 #metaquast assembly/assembly.fasta
-=======
 
-metaquast ./assembly/assembly.fasta -r $( echo reference_genomes/*/*/QC_and_Genome_Assembly/final.assembly.fasta | tr ' ' , )
-#./assembly/assembly.fasta
-#./GCA_003957645.1_BMock12_scaffolds_ILLUMINA_genomic.fna.gz
->>>>>>> 6fd3619f21fefa2dc5a5840057f6d70a45aa05b4
