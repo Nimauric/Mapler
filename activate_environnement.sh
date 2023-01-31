767ee28794349d3206ffa291e42a4c423afc4b2f
Nicolas Maurice <nimauric@cl1n020.genouest.org>
2023-01-31 17:16:51 +0100

    clean up old code for a fresh start for the refactoring

--- activate_environnement.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script activates the environnement created by initialise_environnement.sh
-
-echo "setting up the conda environnement..."
-. /local/env/envconda.sh
-conda activate ~/my_env
