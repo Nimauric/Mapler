99e3a70d134b8476cc6417c633f62cf8373c6db6
Nicolas Maurice <nimauric@cl1n049.genouest.org>
2023-02-07 13:45:22 +0100

    began installing dependancies

---
+++ workflow/scripts/opera_ms_assembler.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# "$1" : name of the short-reads run (SRA accession number)
+# "$2" : path/to/the/R1/short/reads/run.fastq.gz
+# "$3" : path/to/the/R2/short/reads/run.fastq.gz
+
+# "$4" : name of the long-reads run (SRA accession number)
+# "$5" : path/to/the/long/reads/run.fastq.gz
+
+# "$6" : path/to/the/output/folder
+
+generic_arguments = "--no-ref-clustering"
