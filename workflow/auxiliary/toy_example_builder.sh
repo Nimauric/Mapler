3a672ff5b60c0f95b60e0bff777ee99a596dace3
Nicolas Maurice <nimauric@teck.irisa.fr>
2023-02-07 14:46:28 +0100

    theorically finished toy_example_builder

--- workflow/auxiliary/toy_example_builder.sh
+++ workflow/auxiliary/toy_example_builder.sh
@@ -8,16 +8,37 @@
 # "$4" : path to the folder to store temporary files ("/" included)
 # "${@:5}" : paths to reference genomes
 
+# Create merged reference
 path_to_references=( "../data/input_reference_genomes/Halomonassp_3.fasta" "../data/input_reference_genomes/MarspLV10R5108_2.fasta")
 path_to_merged_reference=(../data/merged_reference_genome/toy_merge.fasta)
-
-# Create merged reference
 ./scripts/references_merger.sh $path_to_merged_reference ${path_to_references}
 
 # Fetch the sequencer used for this run
-
+run_name="SRR8073713"
+sequencer=$(./scripts/sequencer_fetcher.sh "$run_name")
+case $sequencer in
+    "PacBio RS II")
+        sequencer_arguments="map-pb"
+        ;;
+    "MinION")
+        sequencer_arguments="map-ont"
+        ;;
+    *)
+        echo "Unsupported or unrecognized read sequencer !"
+        echo $metadata
+        exit 1
+        ;;
+esac
 # Run minimap2
+path_to_run="../data/input_reads/SRR8073713.fastq.gz"
+path_to_sam_align="../data/tmp/toy_mapping_SRR8073713.sam
+minimap2 -ax "$sequencer_arguments" $path_to_merged_reference" "$path_to_run" > "$path_to_sam_align"
 
-# Extract the mapped reads
+
+# Filter the mapped reads
 #samtools view -b -F 4 file.bam > mapped.bam
-#samtools view -F 4 file.bam > mapped.bam
+#samtools view -F 4 file.sam > mapped.sam
+
+# Get reads from the sam
+path_to_output="../data/input_reads/toy_SRR8073713.fastq.gz"
+samtools fastq -F 4 "$path_to_sam_align" "$path_to_output"
