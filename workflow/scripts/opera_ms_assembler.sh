43a0d86dbf163a129fa5ebdad265d42a57c772b8
Nicolas Maurice <nimauric@teck.irisa.fr>
2023-02-13 14:16:38 +0100

    theorically integrated opera ms into the pipeline

--- workflow/scripts/opera_ms_assembler.sh
+++ workflow/scripts/opera_ms_assembler.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-# "$1" : name of the short-reads run (SRA accession number)
-# "$2" : path/to/the/R1/short/reads/run.fastq.gz
-# "$3" : path/to/the/R2/short/reads/run.fastq.gz
+# "$1" : path/to/the/R1/short/reads/run.fastq.gz
+# "$2" : path/to/the/R2/short/reads/run.fastq.gz
 
-# "$4" : name of the long-reads run (SRA accession number)
-# "$5" : path/to/the/long/reads/run.fastq.gz
+# "$3" : path/to/the/long/reads/run.fastq.gz
 
-# "$6" : path/to/the/output/folder
+# "$4" : path/to/the/output/folder
 
 generic_arguments = "--no-ref-clustering"
+mkdir "$4"
+perl dependencies/OPERA-MS/OPERA-MS.pl \
+    --short-read1 "$1" \
+    --short-read2 "$2" \
+    --long-read "$3" \
+    "$generic_arguments" \
+    --out-dir "$4"
