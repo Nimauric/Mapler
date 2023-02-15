1bbcde7eb83b7479c61f4cd5e955bfee1383e625
Nicolas Maurice <nimauric@teck.irisa.fr>
2023-02-15 13:58:26 +0100

    switched support from sippez to unzipped fastq reads inputs

--- workflow/scripts/metaflye_assembler.sh
+++ workflow/scripts/metaflye_assembler.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script assemble a set of reads into a metagenome, using metaflye
 # "$1" : name of the run (SRA accession number)
-# "$2" : path/to/the/run.fastq.gz
+# "$2" : path/to/the/run.fastq
 # "$3" : path/to/the/output/folder
 
 # Fetch the sequencer used for this run
