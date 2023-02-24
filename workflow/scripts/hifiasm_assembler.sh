e8be7009b811d5341d89a3c1140dad6c061d5490
Nicolas Maurice <nimauric@genossh.genouest.org>
2023-02-24 09:33:09 +0100

    bufixes : hifiasm_meta doesn't crash upon starting execution

--- workflow/scripts/hifiasm_assembler.sh
+++ workflow/scripts/hifiasm_assembler.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# This script assemble a set of reads into a metagenome, using miniasm
+# This script assemble a set of reads into a metagenome, using hifiasm_meta
 # "$1" : path/to/the/run.fastq
 # "$2" : path/to/the/output/folder
 
-Ncpu= $(squeue -j "$$" -o \"%C\")
-echo $Ncpu
+mkdir "$2"
 
-hifiasm_meta -o "$2" -t"Ncpu" "$1"
+Ncpu=$(nproc)
+echo hifiasm_meta -o "$2" -t"$Ncpu" "$1"
+hifiasm_meta -o "$2" -t"$Ncpu" "$1"
 
