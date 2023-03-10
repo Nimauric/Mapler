7844747c09da91bca836b8d86491a9a332df42d2
Nicolas Maurice <nimauric@giroflier.irisa.fr>
2023-03-10 10:36:51 +0100

    Added meta_mdbg assembler, as a script and as a rule

---
+++ workflow/scripts/meta_mdbg_assembler.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This script assemble a set of reads into a metagenome, using meta_mdbg
+# "$1" : path/to/the/run.fastq
+# "$2" : path/to/the/output/folder
+
+reads="$1"
+output_folder="$2",
+num_processor=$(nproc)
+
+mkdir "$2"
+metaMDBG asm $output_folder $reads -t $num_processor
