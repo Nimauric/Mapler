7961827952a534a1f9515a353eeb44064a42bcdd
Nicolas Maurice <nimauric@ecogeno03.genouest.org>
2023-04-11 16:41:49 +0200

    final fixes to make checkM work

--- workflow/scripts/checkm.sh
+++ workflow/scripts/checkm.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem-per-cpu=20000
 
 #bins_extensions="fa"
-#input_folder="../metabat_test/"
-#output_folder="./tmp/"
+#input_folder="../data/bins/metaflye_SRR8073714/"
+#output_folder="../data/stats_reports/metaflye_SRR8073714/"
 #out_file="bins_report.tsv"
 
 bins_extension="$1"
@@ -12,15 +12,14 @@ input_folder="$2"
 output_folder="$3"
 out_file="$4"
 
-
 # build folders
 mkdir "$output_folder"
 mkdir "$output_folder"tmp/
 
 # call checkm tree, lineage_set, analyze and qa
-checkm tree "$input_folder" "$output_folder"tmp/ -x "$bins_extensions"
+checkm tree "$input_folder" "$output_folder"tmp/ -x "$bins_extension"
 checkm lineage_set "$output_folder"tmp/ "$output_folder"tmp/markers.txt
-checkm analyze "$output_folder"tmp/markers.txt "$input_folder" "$output_folder"tmp/ -x "$bins_extensions"
+checkm analyze "$output_folder"tmp/markers.txt "$input_folder" "$output_folder"tmp/ -x "$bins_extension"
 checkm qa "$output_folder"tmp/markers.txt "$output_folder"tmp/ --tab_table -f "$output_folder""$out_file"
 
 
