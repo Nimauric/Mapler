c0b2cdb5fddec3c643633cbeb82a5b63db264db6
Nicolas Maurice <nimauric@giroflier.irisa.fr>
2023-04-11 12:09:14 +0200

    theorical automatisation of checkM

--- workflow/scripts/checkm.sh
+++ workflow/scripts/checkm.sh
@@ -2,10 +2,26 @@
 #SBATCH --ntasks=4
 #SBATCH --mem-per-cpu=20000
 
-#input_folder="$1"
-output_folder="$2"
-out_file="$3"
+#bins_extensions="fa"
+#input_folder="../metabat_test/"
+#output_folder="./tmp/"
+#out_file="bins_report.tsv"
 
-checkm lineage_wf ../metabat_test/ ./tmp -x fa
-# tree, lineage_set, analyze, qa
-# Maybe spilt wf into constituent parts to have an idea of the progression and write the qa output in a file ?
+bins_extension="$1"
+input_folder="$2"
+output_folder="$3"
+out_file="$4"
+
+
+# build folders
+mkdir "$output_folder"
+mkdir "$output_folder"tmp/
+
+# call checkm tree, lineage_set, analyze and qa
+checkm tree "$input_folder" "$output_folder"tmp/ -x "$bins_extensions"
+checkm lineage_set "$output_folder"tmp/ "$output_folder"tmp/markers.txt
+checkm analyze "$output_folder"tmp/markers.txt "$input_folder" "$output_folder"tmp/ -x "$bins_extensions"
+checkm qa "$output_folder"tmp/markers.txt "$output_folder"tmp/ --tab_table -f "$output_folder""$out_file"
+
+
+echo "Done !"
