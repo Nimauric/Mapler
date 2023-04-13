fb9467c61a69b3d2143d62ff4ffc81215d9123c4
Nicolas Maurice <nmaurice@devel03.plafrim.cluster>
2023-04-13 14:22:46 +0200

    added runtime in the rules, and fixed a spelling mistake

--- workflow/pipeline.sh
+++ workflow/pipeline.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#SBATCH --ntasks=20
-#SBATCH --mem-per-cpu=100000
-
+#SBATCH --time=24:00:00
 # This script lanches the Snakefile with its correct arguments
-snakemake -pk --jobs 10 --cores all all --use-conda --default-resources mem_mb=5000 mem_mb_per_cpu=None
+snakemake -pk --slurm --jobs 10 --cores all all --use-conda --default-resources mem_mb=5000 mem_mb_per_cpu=None
--- workflow/pipeline_slurm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# This script lanches the Snakefile with its correct arguments
-snakemake -pk --slurm --jobs 10 --cores all all --use-conda --default-resources mem_mb=5000 mem_mb_per_cpu=None
