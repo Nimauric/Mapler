fcf00caa205853b0bd52e9c803a1e405a417e813
Nicolas Maurice <nimauric@giroflier.irisa.fr>
2023-04-13 11:09:43 +0200

    theorical adaptation of reference merger and coverage calculator

--- workflow/pipeline.sh
+++ workflow/pipeline.sh
@@ -3,4 +3,4 @@
 #SBATCH --mem-per-cpu=100000
 
 # This script lanches the Snakefile with its correct arguments
-snakemake -pk --jobs 10 --cores all all --use-conda --default-resources mem_mb=None mem_mb_per_cpu=None
+snakemake -pk --jobs 10 --cores all all --use-conda --default-resources mem_mb=5000 mem_mb_per_cpu=None
