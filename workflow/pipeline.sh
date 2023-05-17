bc9578acc94e5dea6e335060a3e3eb964c17a8c6
Nicolas Maurice <nmaurice@devel03.plafrim.cluster>
2023-05-17 13:05:04 +0200

    binning based stats : rule all only requires stats report, not bins

--- workflow/pipeline.sh
+++ workflow/pipeline.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #SBATCH --time=3-00:00:00
+#SBATCH --mem=500M
 
 # This script lanches the Snakefile with its correct arguments
 snakemake -pk --slurm --jobs 10 --cores all all --use-conda --default-resources mem_mb=5000 mem_mb_per_cpu=None runtime=2*60
