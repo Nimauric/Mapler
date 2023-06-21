48dce77a99539d4f05d914c106958a8b21554840
Nicolas Maurice <nimauric@giroflier.irisa.fr>
2023-06-21 14:51:30 +0200

    typo

--- initialise_env.sh
+++ initialise_env.sh
@@ -1,7 +1,7 @@
 
 mkdir outputs/
-mkdir dependencies/
-mkdir env/
 
 . /local/env/envconda.sh
-mamba create -y -p env/snakemake -c bioconda -c conda-forge snakemake=7.20 mamba
+mamba env create -y -p env/snakemake -c bioconda -c conda-forge snakemake=7.20 mamba
+
+./dependencies/metaMDBG_installer.sh
