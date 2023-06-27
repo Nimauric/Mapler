2cd4ccb1e7aef41cc649ee941d0af04311ae28c4
Nicolas Maurice <nimauric@cl1n031.genouest.org>
2023-06-27 15:16:51 +0200

    operaMS finally launches

--- assemblers/opera_ms_wraper.sh
+++ assemblers/opera_ms_wraper.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# "$1" : path/to/the/R1/short/reads/run.fastq.gz
-# "$2" : path/to/the/R2/short/reads/run.fastq.gz
-
-# "$3" : path/to/the/long/reads/run.fastq.gz
-
-# "$4" : path/to/the/output/folder
 
 short1="$1"
 short2="$2"
@@ -13,28 +7,18 @@ output_directory="$4"
 Ncpu=$(nproc)
 
 echo ======================================
-generic_arguments="--no-ref-clustering --num-processors "$num_processor" "
 
 mkdir -p "$output_directory"
 
 
-echo perl dependencies/OPERA-MS/OPERA-MS.pl \
-    --no-ref-clustering \
-    --num-processors "$num_processor" \
-    --short-read1 "$short1" \
-    --short-read2 "$short2" \
-    --long-read "$long" \
-    --out-dir "$output_directory"/tmp
-
 perl dependencies/OPERA-MS/OPERA-MS.pl \
     --no-ref-clustering \
-    --num-processors "$num_processor" \
+    --num-processors "$Ncpu" \
     --short-read1 "$short1" \
     --short-read2 "$short2" \
     --long-read "$long" \
     --out-dir "$output_directory"/tmp
 
 
-#mv "$4"contigs.polished.fasta  "$4"assembly.fasta
-
+cp "$output_directory"/tmp/contigs.polished.fasta  "$output_directory"/assembly.fasta
 
