c0af35d25727c3c3ff2fdad3f2a94f8350311bd3
MAURICE Nicolas <nicolas.maurice@inria.fr>
2024-08-05 11:36:10 +0200

    data visualisation update, local mode for pipeline

---
+++ local_pipeline.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=mapler 
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=100G
+
+analysis_name=$(grep "analysis_name:" config/config.yaml | cut -d " " -f 2)
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+log_directory=logs/$analysis_name/$timestamp/
+
+# Log config file and git info
+echo "/!\ The exection log isn't saved when run in local mode, please save it manually or if you have access to a slurm cluster, run pipeline.sh instead"
+
+
+mkdir -p $log_directory
+cp config/config.yaml $log_directory/config.yaml
+echo "Branch: $(git rev-parse --abbrev-ref HEAD)" > $log_directory/git_info.txt
+echo "Latest Commit: $(git rev-parse HEAD)" >> $log_directory/git_info.txt
+
+# Launch the pipeline
+snakemake all --cores all --jobs 10 \
+    --printshellcmds --keep-going \
+    --use-conda --rerun-triggers mtime \
+    --configfile $log_directory/config.yaml \
+    --default-resources mem_mb=5000 mem_mb_per_cpu=None runtime=2*60
+
+
