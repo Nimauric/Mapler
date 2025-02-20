5308017e77c4efc9fe783ece2560eb3269a3ec46
MAURICE Nicolas <nicolas.maurice@inria.fr>
2025-02-20 16:33:47 +0100

    automatic OPERA-MS installation

--- sources/assembly/operaMS_wraper.sh
+++ sources/assembly/operaMS_wraper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --time=3-00:00:00
-#SBATCH --mem=100G
-#SBATCH --cpus-per-task=48
+#SBATCH --mem=10G
+#SBATCH --cpus-per-task=1
 
 opera_path="$1"
 long_reads="$2"
@@ -9,6 +9,19 @@ short_reads_1="$3"
 short_reads_2="$4"
 short_read_assembly="$5"
 tmp_directory="$6"
+
+
+if [ ! -d "$opera_path" ]; then
+   echo "not here"
+   mkdir -p "$opera_path"
+   cd "$opera_path"
+   cd ..
+   git clone https://github.com/CSB5/OPERA-MS.git
+   cd OPERA-MS
+   make
+   perl OPERA-MS.pl check-dependency
+fi
+
 
 perl "$opera_path"/OPERA-MS.pl \
     --num-processors $(nproc) \
