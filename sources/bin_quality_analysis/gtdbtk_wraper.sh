497169e4c563593b6ab02f9a961c94d55efa4928
MAURICE Nicolas <nicolas.maurice@inria.fr>
2025-03-10 10:43:02 +0100

    gtdbtk bugfixes

--- sources/bin_quality_analysis/gtdbtk_wraper.sh
+++ sources/bin_quality_analysis/gtdbtk_wraper.sh
@@ -7,13 +7,12 @@ bin_directory="$4"
 
 
 mkdir -p $output_directory/results
-mkdir -p $output_directory/tmp
+
 
 export GTDBTK_DATA_PATH=$gtdbtk_database
 
 gtdbtk classify_wf --cpus $(nproc) \
     --genome_dir $bin_directory \
     --out_dir $output_directory/results \
-    --tmpdir $output_directory/tmp \
     --mash_db $mash_database \
     --extension fa
