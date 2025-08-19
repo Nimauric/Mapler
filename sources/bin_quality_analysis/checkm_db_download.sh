e2b3be387d17385e8f00b0b7ce4cd0cd83353a04
MAURICE Nicolas <nicolas.maurice@inria.fr>
2025-08-19 10:13:29 +0200

    bugfix : incorrect checkm database download

--- sources/bin_quality_analysis/checkm_db_download.sh
+++ sources/bin_quality_analysis/checkm_db_download.sh
@@ -8,7 +8,7 @@ echo ""
 if [ ! -e "$database" ]; then
     parent_dir=$(dirname "$database");
     echo "$parent_dir"/..
-    echo $database
-    mkdir -p $parent_dir
+    echo "$database"
+    mkdir -p "$parent_dir"/..
     checkm2 database --download --path "$parent_dir"/..
 fi
